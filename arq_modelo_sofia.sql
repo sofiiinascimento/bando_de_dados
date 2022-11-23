70b093f541e0e84354b3c1d41db1308f2c05f083
Sofia Nascimento <sofia.n06@aluno.ifsc.edu.br>
2022-11-23 10:06:24 -0300

    Script

--- arq_modelo_sofia.sql
+++ arq_modelo_sofia.sql
@@ -3,10 +3,6 @@
 -- Model: New Model    Version: 1.0
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
@@ -14,8 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE database;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`pessoa`
@@ -29,8 +24,6 @@ CREATE TABLE IF NOT EXISTS `mydb`.`pessoa` (
   `data_nascimento` INT NOT NULL,
   PRIMARY KEY (`cpf`)
   );
-
-
 
 -- -----------------------------------------------------
 -- Table `mydb`.`livros`
@@ -146,8 +139,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`livros_has_editora` (
     REFERENCES `mydb`.`editora` (`codigo_editora`)
   );
 
+DROP DATABASE mydb;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
+/* --------------------------------------- */
+
+INSERT INTO (
+  cpf,
+  nome, 
+  email,
+  nacionalidade
+  sexo,
+  data_nascimento,
+)
+
+VALUES (80222520884,'Carlos Duarte', 'carlos.duarte@gmail.com', 'Brasil','Masculino','02-02-1982')
+       (37406414285, 'Pedro Ricardo Alves','pedroalvess@gmail.com', 'Brasil', 'Masculino','04-24-1991')
+       (96352539076,'Pietra Silva','pietraa123@gmail.com', 'Brasil','França','Feminino','11-03-2000')
+       ()
